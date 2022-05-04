757ebe623514987a352fa500323538b33d294235
Emre Turgut <emre.turgut@trendyol.com>
2022-05-04 22:52:18 +0300

    transaction

--- cli/index.ts
+++ cli/index.ts
@@ -2,16 +2,35 @@ import {
   clusterApiUrl,
   Connection,
   Keypair,
-  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmTransaction,
+  Transaction,
+  TransactionInstruction,
 } from '@solana/web3.js';
+import { BN } from 'bn.js';
 
 const connection = new Connection(clusterApiUrl('devnet'));
 const key = Uint8Array.from([]);
+const programId = new PublicKey('');
 
 async function main() {
   const signer = Keypair.fromSecretKey(key);
-  const balance = await connection.getBalance(signer.publicKey);
-  console.info(`SOL: ${balance / LAMPORTS_PER_SOL}`);
+
+  const data = Buffer.from(Uint8Array.of(0, ...new BN(44).toArray('le', 8)));
+
+  const transaction = new Transaction();
+  transaction.add(
+    new TransactionInstruction({
+      keys: [],
+      programId,
+      data,
+    }),
+  );
+
+  const sig = await sendAndConfirmTransaction(connection, transaction, [
+    signer,
+  ]);
+  console.info(`Sig: ${sig}`);
 }
 
 main().catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use solana_program::{account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey};
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
+use std::convert::TryInto;
 
 entrypoint!(process_instruction);
 fn process_instruction(
@@ -6,9 +9,16 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let key: &u8 = instruction_data.first().unwrap();
+    let (key, rem) = instruction_data.split_first().unwrap();
     match key {
-        0 => msg!("zero"),
+        0 => {
+            let value: u64 = rem
+                .get(1..8)
+                .and_then(|slice| slice.try_into().ok())
+                .map(u64::from_le_bytes)
+                .unwrap_or(0);
+            msg!("zero {}", value)
+        }
         1 => msg!("one"),
         _ => msg!("error"),
     }
